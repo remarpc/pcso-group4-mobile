2136c4fbe8e2218fd35c601caa0d73a48273b911
Remar Caballero <rcaballero@pcso.gov.ph>
2022-05-31 10:24:55 +0800

    LuckyPickPage - Finalize UI components

--- pcso-group4-mobile/pcso-group4-mobile/View/LuckyPickPage.xaml.cs
+++ pcso-group4-mobile/pcso-group4-mobile/View/LuckyPickPage.xaml.cs
@@ -1,15 +1,38 @@
 using pcso_group4_mobile.ViewModel;
+using System.Collections.ObjectModel;
+
 namespace pcso_group4_mobile.View;
 
 public partial class LuckyPickPage : ContentPage
 {
     GamesViewModel viewModel;
+    public List<Games> games = new List<Games>();
 
     public LuckyPickPage(LuckyPickViewModel luckyPickViewModel)
 	{
-		InitializeComponent();
-		BindingContext = luckyPickViewModel;
-	}
+        InitializeComponent();
+        
+        games = new List<Games>()
+        {
+            new Games() { ID = 1, Game = 642 },
+            new Games() { ID = 2, Game = 645 },
+            new Games() { ID = 3, Game = 649 },
+            new Games() { ID = 4, Game = 655 },
+            new Games() { ID = 5, Game = 658 }
+        };
+
+
+        //picker_1.SetBinding(Picker.ItemsSourceProperty, "games");
+        //picker_1.ItemDisplayBinding = new Binding("Game");
+        //picker_1.ItemsSource = games;
+        
+
+        BindingContext = luckyPickViewModel;
+        //picker_1.ItemDisplayBinding = new Binding("Game");
+        //picker_1.BindingContext = games;
+
+
+    }
 
     private void Picker_SelectedIndexChanged(object sender, EventArgs e)
     {
