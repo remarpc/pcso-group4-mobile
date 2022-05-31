c0a191baa18cc178058fe67b681c97d20a5714d9
Remar Caballero <rcaballero@pcso.gov.ph>
2022-05-31 14:35:38 +0800

    CombinationPage - Update UI, get user entries and save to object

--- pcso-group4-mobile/pcso-group4-mobile/ViewModel/CombinationsViewModel.cs
+++ pcso-group4-mobile/pcso-group4-mobile/ViewModel/CombinationsViewModel.cs
@@ -1,12 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace pcso_group4_mobile.ViewModel
 {
-    public partial class CombinationsViewModel : BaseViewModel
+    public partial class CombinationsViewModel : BaseViewModel , INotifyPropertyChanged
     {
+        public CombinationsViewModel()
+        {
+            PostCombination = new Command(AddCombination);
+            var c = new Combination()
+            {
+                GameID = GameId,
+                Number1 = Convert.ToInt16(Num1),
+                Number2 = Convert.ToInt16(Num2),
+                Number3 = Convert.ToInt16(Num3),
+                Number4 = Convert.ToInt16(Num4),
+                Number5 = Convert.ToInt16(Num5),
+                Number6 = Convert.ToInt16(Num6)
+            };
+
+        }
+
+        int id;
+        public int Id 
+        {
+            get => id; 
+            set
+            {
+                id = value;
+                OnPropertyChanged(nameof(id));
+            }
+        }
+        public int GameId { get; set; }
+        public string Num1 { get; set; }
+        public string Num2 { get; set; }
+        public string Num3 { get; set; }
+        public string Num4 { get; set; }
+        public string Num5 { get; set; }
+        public string Num6 { get; set; }
+
+        public ICommand PostCombination { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged(string i) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(i));
+
+        private void AddCombination()
+        {
+            Combination combination = new Combination();
+            Combination c = new Combination() {
+                GameID = GameId,
+                Number1 = Convert.ToInt16(Num1),
+                Number2 = Convert.ToInt16(Num2), 
+                Number3 = Convert.ToInt16(Num3), 
+                Number4 = Convert.ToInt16(Num4), 
+                Number5 = Convert.ToInt16(Num5), 
+                Number6 = Convert.ToInt16(Num6)
+            };
+
+            // Clear
+            Num1 = ""; Num2 = ""; Num3 = ""; Num4 = ""; Num5 = ""; Num6 = "";
+            OnPropertyChanged(nameof(Num1));
+            OnPropertyChanged(nameof(Num2));
+            OnPropertyChanged(nameof(Num3));
+            OnPropertyChanged(nameof(Num4));
+            OnPropertyChanged(nameof(Num5));
+            OnPropertyChanged(nameof(Num6));
+        }
     }
 }
