199af5e7bbaf82693b5f7c94fd4a0e8b619cf13f
Richard Verdera, Jr <rverdera@pcso.gov.ph>
2022-06-01 09:33:09 +0800

    Add model and service folder

---
+++ pcso-group4-mobile/pcso-group4-mobile/Model/GameModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pcso_group4_mobile.Model;
+
+public class GameModel
+{
+    public int ID { get; set; }
+    public int Game { get; set; }
+}
---
+++ pcso-group4-mobile/pcso-group4-mobile/Service/ServiceClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using pcso_group4_mobile.Model;
+
+namespace pcso_group4_mobile.Service;
+
+public class ServiceClient
+{
+    public const string url = "https://pcso-group4-backend.azurewebsites.net/";
+    public static async Task<List<GameModel>> GetGames()
+    {
+        try
+        {
+            HttpClient client = new HttpClient();           
+            string response = await client.GetStringAsync(url + "/gameitems");
+            List<GameModel> games = JsonConvert.DeserializeObject<List<GameModel>>(response);
+            return games;
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    
+    }
+}
